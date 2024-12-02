75e96df1c3a9aa17244a9949c69cb92a7addda04
Edison Liang <edisonliang@lev-00326.apn.wlan.private.upenn.edu>
2024-12-02 04:35:06 -0500

    q2

--- q2/q2.py
+++ q2/q2.py
@@ -4,7 +4,7 @@ lines = []
 
 with open(fp, "r") as file:
     for line in file:
-        line = line.split()
+        line = list(map(int, line.split()))
         lines.append(line)
 
 
@@ -12,28 +12,43 @@ def part1():
     res = 0
 
     for line in lines:
-        success = True
-        if line[1] > line[0]:
-            increasing = True
-        elif line[1] < line[0]:
-            increasing = False
-        else:
-            # neither increasing or decreasing
-            continue
-
-        for i in range(1, len(line)):
-            if not (1 <= abs(int(line[i]) - int(line[i - 1])) <= 3):
-                success = False
-                break
-
-            if increasing and not line[i] > line[i - 1] or not increasing and not line[i] < line[i - 1]:
-                success = False
-                break
-
-        if success:
+        if checkValidLine(line):
             res += 1
 
     print(res)
 
 
-part1()
+def part2():
+    res = 0
+
+    for line in lines:
+        valid = False
+        for i in range(len(line)):
+            if checkValidLine(line[:i] + line[i + 1:]):
+                valid = True
+                break
+        if valid:
+            res += 1
+
+    print(res)
+
+
+def checkValidLine(line):
+    if line[1] > line[0]:
+        increasing = True
+    elif line[1] < line[0]:
+        increasing = False
+    else:
+        return False
+
+    for i in range(1, len(line)):
+        if not (1 <= abs(line[i] - line[i - 1]) <= 3):
+            return False
+
+        if increasing and not line[i] > line[i - 1] or not increasing and not line[i] < line[i - 1]:
+            return False
+
+    return True
+
+
+part2()
---
+++ q3/q3.py
@@ -0,0 +1,13 @@
+fp = "dataQ3.txt"
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+part1()
+part2()
